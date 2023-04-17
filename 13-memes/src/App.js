5dc9c23c93961d3790c73d4662358b6da2d4d486
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2023-04-17 16:06:40 -0300

    Clase 13: Instalación SweetAlert2 y html2canvas

--- 13-memes/src/App.js
+++ 13-memes/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Cookies from './Componentes/Cookies';
 import Imgmeme from './Componentes/Imgmeme';
+
 
 function App() {
   return (
     <div className="App">
-
+    
+    <Cookies/>
     <Imgmeme />
 
     </div>
